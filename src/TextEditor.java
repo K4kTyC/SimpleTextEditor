d799e309d0a0114275bc46e6e78a618949cd87b8
KakTyC <47088033+K4kTyC@users.noreply.github.com>
2020-07-23 22:14:21 +0300

    Added search buttons, changed load/save icons

--- src/TextEditor.java
+++ src/TextEditor.java
@@ -13,14 +13,10 @@ public class TextEditor extends JFrame {
     public TextEditor() {
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setTitle("Simple Text Editor");
-        setSize(500, 700);
+        setSize(600, 700);
+        setLocation(600, 200);
         Font font = new Font("Courier", Font.PLAIN, 16);
 
-
-        JTextField textField = new JTextField();
-        textField.setName("FilenameField");
-        textField.setPreferredSize(new Dimension(100, 30));
-        textField.setFont(font.deriveFont(14f));
 
         JTextArea textArea = new JTextArea();
         textArea.setName("TextArea");
@@ -33,29 +29,57 @@ public class TextEditor extends JFrame {
         loadButton.setName("LoadButton");
         loadButton.setPreferredSize(new Dimension(38, 38));
         loadButton.addActionListener(event -> {
-            textArea.setText(null);
+            /*textArea.setText(null);
             String fileName = textField.getText();
             try {
                 String dataFromFile = new String(Files.readAllBytes(Paths.get(fileName)));
                 textArea.setText(dataFromFile);
             } catch (IOException ioException) {
                 System.out.println("Error: " + ioException.getMessage());
-            }
+            }*/
         });
 
         JButton saveButton = new JButton(new ImageIcon("res/icons/saveIcon.png"));
         saveButton.setName("SaveButton");
         saveButton.setPreferredSize(new Dimension(38, 38));
         saveButton.addActionListener(event -> {
-            String fileName = textField.getText();
+            /*String fileName = textField.getText();
             File targetFile = new File(fileName);
             String dataToFile = textArea.getText();
             try (FileWriter writer = new FileWriter(targetFile)) {
                 writer.write(dataToFile);
             } catch (IOException ioException) {
                 System.out.println("Error: " + ioException.getMessage());
-            }
+            }*/
         });
+
+        JTextField searchField = new JTextField();
+        searchField.setName("SearchField");
+        searchField.setFont(font.deriveFont(18f));
+
+        JButton searchButton = new JButton(new ImageIcon("res/icons/searchIcon.png"));
+        searchButton.setName("StartSearchButton");
+        searchButton.setPreferredSize(new Dimension(38, 38));
+        searchButton.addActionListener(event -> {
+            // search operation
+        });
+
+        JButton prevMatchButton = new JButton(new ImageIcon("res/icons/prevMatchIcon.png"));
+        prevMatchButton.setName("PreviousMatchButton");
+        prevMatchButton.setPreferredSize(new Dimension(38, 38));
+        prevMatchButton.addActionListener(event -> {
+            // search operation
+        });
+
+        JButton nextMatchButton = new JButton(new ImageIcon("res/icons/nextMatchIcon.png"));
+        nextMatchButton.setName("NextMatchButton");
+        nextMatchButton.setPreferredSize(new Dimension(38, 38));
+        nextMatchButton.addActionListener(event -> {
+            // search operation
+        });
+
+        JCheckBox useRegexBox = new JCheckBox("Use regex");
+        useRegexBox.setFont(font.deriveFont(18f));
 
         JPanel filePane = new JPanel();
         filePane.setLayout(new BoxLayout(filePane, BoxLayout.LINE_AXIS));
@@ -64,7 +88,15 @@ public class TextEditor extends JFrame {
         filePane.add(Box.createRigidArea(new Dimension(10, 0)));
         filePane.add(saveButton);
         filePane.add(Box.createRigidArea(new Dimension(10, 0)));
-        filePane.add(textField);
+        filePane.add(searchField);
+        filePane.add(Box.createRigidArea(new Dimension(10, 0)));
+        filePane.add(searchButton);
+        filePane.add(Box.createRigidArea(new Dimension(10, 0)));
+        filePane.add(prevMatchButton);
+        filePane.add(Box.createRigidArea(new Dimension(10, 0)));
+        filePane.add(nextMatchButton);
+        filePane.add(Box.createRigidArea(new Dimension(10, 0)));
+        filePane.add(useRegexBox);
 
         JPanel textPane = new JPanel();
         textPane.setLayout(new BorderLayout());
@@ -80,27 +112,27 @@ public class TextEditor extends JFrame {
         JMenuItem loadMenuItem = new JMenuItem("Load");
         loadMenuItem.setName("MenuLoad");
         loadMenuItem.addActionListener(event -> {
-            textArea.setText(null);
+            /*textArea.setText(null);
             String fileName = textField.getText();
             try {
                 String dataFromFile = new String(Files.readAllBytes(Paths.get(fileName)));
                 textArea.setText(dataFromFile);
             } catch (IOException ioException) {
                 System.out.println("Error: " + ioException.getMessage());
-            }
+            }*/
         });
 
         JMenuItem saveMenuItem = new JMenuItem("Save");
         saveMenuItem.setName("MenuSave");
         saveMenuItem.addActionListener(event -> {
-            String fileName = textField.getText();
+            /*String fileName = textField.getText();
             File targetFile = new File(fileName);
             String dataToFile = textArea.getText();
             try (FileWriter writer = new FileWriter(targetFile)) {
                 writer.write(dataToFile);
             } catch (IOException ioException) {
                 System.out.println("Error: " + ioException.getMessage());
-            }
+            }*/
         });
 
         JMenuItem exitMenuItem = new JMenuItem("Exit");
